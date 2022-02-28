96343b690655e3d43893bf3cb7c539a5aa8d1f54
TuHechiceroFavorito <64440523+TuHechiceroFavorito@users.noreply.github.com>
2022-02-28 19:03:53 +0000

    Get the response into a file and reading data

--- maps_api_interfacce.py
+++ maps_api_interfacce.py
@@ -3,40 +3,51 @@ Simple Program to help you get started with Google's APIs
 """
 import urllib.request, json, os, sys
 
-# Read the API key from the file
-with open("key.txt", "r") as f:
-    key = f.read()
+def request_api():
+    # Read the API key from the file
+    with open("key.txt", "r") as f:
+        key = f.read()
 
-#Sends the request and reads the response.
-#response = urllib.request.urlopen("https://maps.googleapis.com/maps/api/directions/json?").read()
-#Loads response as JSON
+    #Sends the request and reads the response.
+    #response = urllib.request.urlopen("https://maps.googleapis.com/maps/api/directions/json?").read()
+    #Loads response as JSON
 
-body = "https://maps.googleapis.com/maps/api/directions/json?"
-origin = "origin=53.306292,-6.218746"
-destination = "&destination=53.303965,-6.217158"
-mode = "&mode=walking"
-language = "&language=en"
-alternatives = "&alternatives=false"
-units = "&units=metric"
-api_key = "&key=" + key
+    body = "https://maps.googleapis.com/maps/api/directions/json?"
+    origin = "origin=53.306292,-6.218746"
+    destination = "&destination=53.303965,-6.217158"
+    mode = "&mode=walking"
+    language = "&language=en"
+    alternatives = "&alternatives=false"
+    units = "&units=metric"
+    api_key = "&key=" + key
 
 
-response = body + origin + destination + mode + language + alternatives + units + api_key
-response_final = urllib.request.urlopen(response).read()
+    response = body + origin + destination + mode + language + alternatives + units + api_key
+    response_final = urllib.request.urlopen(response).read()
 
-directions = json.loads(response_final)
-sys.stdout = open("response.txt", "w")
+    with open("response.json", "w") as f:
+        f.write(response_final.decode("utf-8"))
 
-i=1
 
-for x in directions["routes"][0]["legs"][0]["steps"]:
-    # if x == "distance":
-    print(f"distance {x['distance']}") 
-    print(f"duration {x['duration']}")    
-    print(f"end_location {x['end_location']}")
-    print(f"start_location {x['start_location']}\n")
-    print(f"***Node {i} has been reached***\n")
+def test_reading():
+    with open("response.json", "r") as f:
+        response = f.read()
 
-    i+=1
+    directions = json.loads(response)
 
-sys.stdout.close()
+
+    i=1
+
+    for x in directions["routes"][0]["legs"][0]["steps"]:
+        # if x == "distance":
+        print(f"distance {x['distance']}") 
+        print(f"duration {x['duration']}")    
+        print(f"end_location {x['end_location']}")
+        print(f"start_location {x['start_location']}\n")
+        print(f"***Node {i} has been reached***\n")
+
+        i+=1
+
+test_reading()
+
+# sys.stdout.close()
